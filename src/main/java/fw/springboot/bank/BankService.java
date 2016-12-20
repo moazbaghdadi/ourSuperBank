c30adc29a78dd53f6fdc5d1cc88fa6e8d25b807d
Sulser <fabio.sulser@accenture.com>
2016-12-20 16:11:43 +0100

    added functions to BankService Interface

--- src/main/java/fw/springboot/bank/BankService.java
+++ src/main/java/fw/springboot/bank/BankService.java
@@ -1,6 +1,18 @@
 package fw.springboot.bank;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface BankService {
 
 	public BankAccount createAccount();
+	
+	public List<BankAccount> getAllAccounts();
+	
+	public BankAccount getOneAccount();
+	
+	public BigDecimal getBalance(String accountNumber);
+	
+	public BigDecimal book(String accountNumber, BigDecimal amount);
+	
 }
