1b92a73ecab3639e7b2e6d6034aeb7444498a1d1
Kraus <guenter.kraus@accenture.com>
2016-12-20 16:16:01 +0100

    add missing stubs to BankServiceImpl

--- src/main/java/fw/springboot/bank/BankServiceImpl.java
+++ src/main/java/fw/springboot/bank/BankServiceImpl.java
@@ -1,5 +1,8 @@
 package fw.springboot.bank;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,6 +10,30 @@ public class BankServiceImpl implements BankService {
 
 	@Override
 	public BankAccount createAccount() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BankAccount> getAllAccounts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BankAccount getOneAccount(String accountNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BigDecimal getBalance(String accountNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BigDecimal book(String accountNumber, BigDecimal amount) {
 		// TODO Auto-generated method stub
 		return null;
 	}
