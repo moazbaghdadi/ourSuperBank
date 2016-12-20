be8ea5923e52950bae4116da18e457291f9b7294
Kraus <guenter.kraus@accenture.com>
2016-12-20 16:15:28 +0100

    add BankServiceImpl with stubs

---
+++ src/main/java/fw/springboot/bank/BankServiceImpl.java
@@ -0,0 +1,14 @@
+package fw.springboot.bank;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BankServiceImpl implements BankService {
+
+	@Override
+	public BankAccount createAccount() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
