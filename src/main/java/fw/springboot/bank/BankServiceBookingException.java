ea87fe0389ad750fb5b51a54ee9edb9ec8179f12
Kraus <guenter.kraus@accenture.com>
2016-12-21 12:31:14 +0100

    add BookingExceptionClass for bookError

---
+++ src/main/java/fw/springboot/bank/BankServiceBookingException.java
@@ -0,0 +1,30 @@
+package fw.springboot.bank;
+
+public class BankServiceBookingException extends RuntimeException {
+
+	public BankServiceBookingException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public BankServiceBookingException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BankServiceBookingException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BankServiceBookingException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BankServiceBookingException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
