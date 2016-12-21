16137b6e8190c0ae3a143342914a42ed17b0b294
moaz.baghdadi <moaz.baghdadi@CPX-3WNETTZAO92.dir.svc.accenture.com>
2016-12-21 15:53:34 +0100

    add SpringControllerConfig

---
+++ src/main/java/fw/springboot/bank/SpringContollerConfig.java
@@ -0,0 +1,23 @@
+package fw.springboot.bank;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+public class SpringContollerConfig {
+	
+	@Bean
+	public WebMvcConfigurer corseConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
+}
