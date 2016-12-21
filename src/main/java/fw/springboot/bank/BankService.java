91abc7f1d127a9be6438dc7d7dc08ceef965a732
Kraus <guenter.kraus@accenture.com>
2016-12-21 14:34:17 +0100

    added comments to interface methods

--- src/main/java/fw/springboot/bank/BankService.java
+++ src/main/java/fw/springboot/bank/BankService.java
@@ -5,14 +5,32 @@ import java.util.List;
 
 public interface BankService {
 
+	/**
+	 * creates a new BankAccount
+	 * @return BankAccount new instance with an Account number
+	 */
 	public BankAccount createAccount();
-	
+
+	/**
+	 * Queries BankRepository to retrieve all BankAccounts of this Bank
+	 * @return list of all BankAccounts in the Database of Bank
+	 */
 	public List<BankAccount> getAllAccounts();
-	
+
+	/**
+	 * queries BankRepository through accountNumber to get the BankAccount instance 
+	 * @param accountNumber number of the BankAccount
+	 * @return an instance of BankAccount except accountNumber is invalid, then null
+	 */
 	public BankAccount getOneAccount(String accountNumber);
-	
+
+	/**
+	 * queries BankRepository through accountNumber to get the BankAccount instance and then retrieves the Balance from the Account
+	 * @param accountNumber number of the BankAccount
+	 * @return a BigDecimal instance with the value of the balance from the BankAccount
+	 */
 	public BigDecimal getBalance(String accountNumber);
-	
+
 	public BigDecimal book(String accountNumber, BigDecimal amount);
-	
+
 }
